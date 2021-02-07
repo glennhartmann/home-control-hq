f67c08137b9dc49bc2e86f7bd8a1494077666c63
Peter Beverloo <pbeverloo@gmail.com>
2021-02-07 17:10:02 +0000

    Merge the HTTP Server in the Network component

--- src/network/network.ts
+++ src/network/network.ts
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import { Server, ServerOptions } from './server';
+import express, { NextFunction, Request, Response } from 'express';
+import expressWs, { Application } from 'express-ws';
+import * as ws from 'ws';
+
 import { Logger } from '../base/logger';
 
 // The delegate that defines interaction of the network stack with the component that owns it.
@@ -11,24 +14,70 @@ export interface NetworkDelegate {
 }
 
 // Dictionary containing the required options when initializing the network environment.
-export type NetworkOptions = ServerOptions;
+export interface NetworkOptions {
+    // Hostname on which the HTTP server should listen.
+    hostname: string;
 
-// Driver for networking-related functionality of the smart home control system. Launches HTTP and
-// WebSocket servers at the configured ports and provides the ability to interact with them.
+    // Port number on which the HTTP server should listen.
+    port: number;
+
+    // Qualified path to the `public` directory for static files.
+    public?: string;
+}
+
+// Provides the HTTP server, backed by ExpressJS, through which the frontend of the smart home
+// system will be served to the devices. HTTPS is not used because, well, it's local, and if someone
+// has access to our network then they're welcome to the light switches too :)
+//
+// The HTTP server further exposes a /control endpoint, which can be connected to through the
+// WebSockets protocol for long-lived bidirectional communication with the clients.
+//
+// The Network interface provides the ability to broadcast messages, optionally filtered for clients
+// interested in a particular room, and communicates with the rest of the server through the
+// delegate interface, for example to propagate command messages.
 export class Network {
     private delegate: NetworkDelegate;
     private logger: Logger;
+    private options: NetworkOptions;
 
-    private server: Server;
+    private server: Application;
 
     constructor(delegate: NetworkDelegate, logger: Logger, options: NetworkOptions) {
         this.delegate = delegate;
         this.logger = logger;
+        this.options = options;
 
-        this.server = new Server(logger, options);
+        this.server = expressWs(express()).app;
+
+        this.server.use(Network.prototype.onNetworkRequest.bind(this));
+        this.server.ws('/control', Network.prototype.onNetworkConnection.bind(this));
+
+        if (options.public)
+            this.server.use(express.static(options.public));
     }
 
+    // ---------------------------------------------------------------------------------------------
+    // Interface for listening to incoming requests
+    // ---------------------------------------------------------------------------------------------
+
+    // Called when any network request has been issued to the server.
+    onNetworkRequest(request: Request, response: Response, next: NextFunction) {
+        this.logger.debug(`Network request from ${request.ip}: ${request.path}`);
+        next();
+    }
+
+    // Called when a WebSocket connection with the `/control` endpoint has been initiated.
+    onNetworkConnection(webSocket: ws, request: Request, next: NextFunction) {
+        this.logger.debug(`WebSocket connection initiated by ${request.ip}.`);
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
     listen() {
-        this.server.listen();
+        const { hostname, port } = this.options;
+
+        this.server.listen(port, hostname, () => {
+            this.logger.info(`The HTTP server is now listening on ${hostname}:${port}...`);
+        });
     }
 }
